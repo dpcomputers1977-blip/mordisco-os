458860ae64a805a4a613f787268b3b6b9e75cfc0
dpcomputers1977-blip <dpcomputers1977@gmail.com>
2026-07-30 14:55:43 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mordisco-client-v8';
+const CACHE='mordisco-client-v13';
 const ASSETS=['/app','/app.html','/client-app.css?v=8.0.0','/client-app.js?v=8.0.0','/mordisco-logo.png','/icon-192.png','/icon-512.png','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
